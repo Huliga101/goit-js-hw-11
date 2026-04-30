deccf946e8768e171162ef7ebdcc4a191461bc60
Daniil_Renhevych19 <renden20012@gmail.com>
2026-04-30 09:45:55 +0200

    HW-11: image search
    реалізував пошук через Pixabay API
    додав галерею з SimpleLightbox
    обробка помилок і loader

---
+++ src/js/render-functions.js
@@ -0,0 +1,27 @@
+export function createGalleryMarkup(images) {
+  return images
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `
+      <li class="gallery-item">
+        <a class="gallery-link" href="${largeImageURL}">
+          <img class="gallery-image" src="${webformatURL}" alt="${tags}" />
+        </a>
+        <div class="gallery-info">
+          <p><b>Likes</b>${likes}</p>
+          <p><b>Views</b>${views}</p>
+          <p><b>Comments</b>${comments}</p>
+          <p><b>Downloads</b>${downloads}</p>
+        </div>
+      </li>
+    `
+    )
+    .join('');
+}
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import axios from 'axios';
+import fetchData from './js/pixabay-api'
+import fetchData from './js/render-functions'
 
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
@@ -6,10 +7,10 @@ import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const API_KEY = '55655169-5dca28bc7cc616be385f48ac4';
-const BASE_URL = 'https://pixabay.com/api/';
+import { getImagesByQuery } from './js/pixabay-api.js';
+import { createGalleryMarkup } from './js/render-functions.js';
 
-const form = document.querySelector('.search-form');
+const form = document.querySelector('.form');
 const gallery = document.querySelector('.gallery');
 const loader = document.querySelector('.loader');
 
@@ -24,87 +25,38 @@ form.addEventListener('submit', async event => {
   const query = form.elements.searchQuery.value.trim();
 
   if (!query) {
-    showError('Please enter a search query');
+    iziToast.error({
+      message: 'Please enter a search query',
+      position: 'topRight',
+    });
     return;
   }
 
   gallery.innerHTML = '';
-  showLoader();
+  loader.classList.remove('hidden');
 
   try {
-    const images = await fetchImages(query);
+    const images = await getImagesByQuery(query);
 
     if (images.length === 0) {
-      showError(
-        'Sorry, there are no images matching your search query. Please try again!'
-      );
+      iziToast.error({
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+        position: 'topRight',
+      });
       return;
     }
 
     gallery.innerHTML = createGalleryMarkup(images);
     lightbox.refresh();
   } catch {
-    showError('Something went wrong. Please try again later.');
+    iziToast.error({
+      message: 'Something went wrong. Please try again later.',
+      position: 'topRight',
+    });
   } finally {
-    hideLoader();
+    loader.classList.add('hidden');
   }
+
+  form.reset();
 });
-
-async function fetchImages(query) {
-  const { data } = await axios.get(BASE_URL, {
-    params: {
-      key: API_KEY,
-      q: query,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: true,
-    },
-  });
-
-  return data.hits;
-}
-
-function createGalleryMarkup(images) {
-  return images
-    .map(
-      ({
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) => `
-      <li class="gallery-item">
-        <a class="gallery-link" href="${largeImageURL}">
-          <img class="gallery-image" src="${webformatURL}" alt="${tags}" />
-        </a>
-        <div class="image-info">
-          <p><b>Likes</b>${likes}</p>
-          <p><b>Views</b>${views}</p>
-          <p><b>Comments</b>${comments}</p>
-          <p><b>Downloads</b>${downloads}</p>
-        </div>
-      </li>
-    `
-    )
-    .join('');
-}
-
-function showLoader() {
-  loader.classList.remove('is-hidden');
-}
-
-function hideLoader() {
-  loader.classList.add('is-hidden');
-}
-
-function showError(message) {
-  iziToast.error({
-    message,
-    position: 'topRight',
-  });
-}
-
-console.log(images);
